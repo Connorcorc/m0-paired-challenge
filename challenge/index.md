29b5a37572fc5fafca2beb33b10aff7c46d98711
Connor Corcoran <connorcorc@gmail.com>
2022-04-18 20:23:30 -0600

    Add JS & index

---
+++ challenge/index.md
@@ -0,0 +1,10 @@
+# 5 Book Titles
+
+1. Too Fat Fish
+2. Harry Potter
+3. Calvin & Hobbes
+4. Notebook
+5. Other Book
+
+Morgan and I collaborated and got through this together! I had a syntax error
+when testing my code, but all worked out in the end. I also looked up syntax for markdown headers...
---
+++ challenge/javascript.js
@@ -0,0 +1,13 @@
+// It's a javascript file....
+
+var firstString = 'Bananas';
+var secondString = 'Apples';
+var firstNum = 30;
+var secondNum = 50;
+var isTrue = true;
+var notTrue = false;
+
+var books = ['Cat in the hat', 'One fish, two fish', "Green eggs & ham"];
+
+console.log('I like ' + firstString + ' and also ' + secondString + '. I am ' + firstNum + ' but I feel ' +
+secondNum + '.')
